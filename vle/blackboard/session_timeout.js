d0fec885becffb169822dfff022e381baad0ee37
Miroslav Kratchounov <miroslav.kratchounov@blackboard.com>
2016-01-11 17:17:09 +0100

    Added Blackboard VLE examples.

---
+++ vle/blackboard/session_timeout.js
@@ -0,0 +1,47 @@
+{
+    "version": "1.0.0",
+    "id": "fed1365a-61c7-4785-a07f-8ef4837695f8",
+    "timestamp": "2015-12-10T08:55:09.000Z",
+    "verb": {
+        "id": "https://w3id.org/xapi/adl/verbs/abandoned",
+        "display": {
+            "en-US": "session timed out"
+        }
+    },
+    "actor": {
+        "objectType": "Agent",
+        "name": "test1 test1",
+        "account": {
+            "homePage": "https://services-uswest.skytap.com:27219",
+            "name": "test1"
+        }
+    },
+    "object": {
+        "objectType": "Activity",
+        "id": "https://services-uswest.skytap.com:27219/webapps/login/",
+        "definition": {
+            "name": {
+                "en-US": "Blackboard (https://services-uswest.skytap.com:27219)"
+            },
+            "description": {
+                "en-US": "Blackboard (https://services-uswest.skytap.com:27219)"
+            }
+        }
+    },
+    "context": {
+        "platform": "Blackboard",
+        "extensions": {
+            "http://lrs.learninglocker.net/define/extensions/info": {
+                "https://services-uswest.skytap.com:27219": "9.1.201410",
+                "https://services-uswest.skytap.com:27219/webapps/bbc-xAPI-BBLEARN/": "1.0.0",
+                "ip": "192.168.0.1"
+            }
+        }
+    },
+    "authority": {
+        "objectType": "Agent",
+        "name": "BB Learn",
+        "mbox": "mailto:hello@learninglocker.net"
+    },
+    "stored": "2016-01-08T13:10:39.418100+00:00"
+}
